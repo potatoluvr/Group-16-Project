5f4b33785fcf88317950c9831b66e004ff33b41d
MagicalCandyBar <jfkriekenbeek@gmail.com>
2025-02-14 01:08:21 -0600

    Added 404 Page not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./webpages/Login";
 import SignUp from "./webpages/SignUp";
 import UserProfileManagement from "./webpages/UserProfileManagement";
+import NotFound from "./components/404";
 import "./components/FooterStyle.css";
 import "./components/HeaderStyle.css";
 
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/user-profile" element={<UserProfileManagement />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
