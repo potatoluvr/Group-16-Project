3c7819522a7a2938f0c8541ecc37c3465f27ef6d
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-03-07 09:21:22 -0600

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import volunteerRoutes from "./routes/volunteerRoutes.js";
 
 // Load environment variables
 dotenv.config();
@@ -14,6 +17,9 @@ app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/volunteers", volunteerRoutes);
 
 // Start the server
 if (process.env.NODE_ENV !== "test") {
