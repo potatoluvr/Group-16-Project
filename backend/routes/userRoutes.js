883a6827970203f34b69d420c13b7811a1f866bb
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-03-07 09:09:56 -0600

    created userRoutes.js

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { getUserProfile, updateUserProfile } = require("../controllers/userController");
+
+const router = express.Router();
+
+// Route to fetch user profile
+router.get("/:id", getUserProfile);
+
+// Route to update user profile
+router.post("/update", updateUserProfile);
+
+module.exports = router;
