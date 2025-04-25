a8642db9698b41f9e82e188bee08ff46ef11ce4d
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-04-25 03:17:27 -0500

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,7 +39,7 @@ const users = [
 // Get User Profile
 export async function getUserProfile(req, res) {
   try {
-    const userId = req.params;
+    const userId = req.params.id;
 
     const profile = await UserProfile.findOne({
       userId: new Types.ObjectId(userId),
