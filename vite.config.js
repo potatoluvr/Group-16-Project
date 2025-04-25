b6932174b4594ae2e188ef124f9ad80dc6d5ff5e
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-04-24 20:52:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Terminal from 'vite-plugin-terminal'
 
-const isDev = process.env.NODE_ENV === 'development'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
