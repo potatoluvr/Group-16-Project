c7ec9b10d39ed02aa0b23e13fd12e0bebd44245a
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-04-25 14:41:19 -0500

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { createEvent, getEvents } from "../controllers/eventController.js";
+import verifyToken, { isAdmin } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
+router.post("/event", verifyToken, isAdmin, createEvent); 
 router.post("/create-event", createEvent);
 router.get("/", getEvents);
 
