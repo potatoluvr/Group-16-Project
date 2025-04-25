6cc8a0241fa2406c2ca337d13a81226f19ada714
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-04-25 01:14:06 -0500

    Update SignUpForm.jsx

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -47,7 +47,7 @@ function SignUpForm() {
 
     try {
       // Send data to the back end using fetch
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
