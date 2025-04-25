ba3f9eaf9363c5b970118a4901c303f18064a956
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-04-25 02:28:37 -0500

    Update UserProfileManagementForm.jsx

--- src/components/UserProfileManagementForm.jsx
+++ src/components/UserProfileManagementForm.jsx
@@ -109,7 +109,7 @@ function UserProfileForm() {
         };
   
         const response = await fetch(`${import.meta.env.VITE_API_URL}/api/users/profile`, {
-          method: "POST",
+          method: "PUT",
           headers: {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
