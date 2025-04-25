b9af565b38948ddc9899c95c94538bb6aa7d2797
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-04-25 01:06:29 -0500

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -58,6 +58,7 @@ export const loginUser = async (req, res) => {
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
+    console.log("Password match:", isMatch);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
